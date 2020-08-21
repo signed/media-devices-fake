026e5d9386f78331b60b0f5cf667510054055612
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-08-21 23:46:25 +0200

    export the scenarios for now

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 export { MediaDevicesFake } from './MediaDevicesFake';
 export { MediaDeviceDescription } from './MediaDeviceDescription';
+
+// todo testrig should be moved here
+export { MediaStreamCheckResult, Scenario } from './Scenarios';
+import { scenarios as all, passUndefined, existingDevice, allConstraintsFalse, requestedDeviceTypeNotAttached, noDeviceWithDeviceId } from './Scenarios';
+
+export const scenarios = {
+    all,
+    passUndefined,
+    existingDevice,
+    allConstraintsFalse,
+    requestedDeviceTypeNotAttached,
+    noDeviceWithDeviceId
+};
