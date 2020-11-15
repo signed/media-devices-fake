5f31519fde93a7c636df1e63f1c886f4e2c69368
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-11-15 23:37:22 +0100

    improve error message

--- src/matchers/dom-exception.ts
+++ src/matchers/dom-exception.ts
@@ -10,12 +10,13 @@ declare global {
 
 expect.extend({
     domException(received: DOMException, msg: string = '', name: string = 'Error'): jest.CustomMatcherResult {
+        const constructor = Object.getPrototypeOf(received).constructor.name;
         const passType = received instanceof DOMException;
         const passMessage = received.message === msg;
         const passName = received.name === name;
         const pass: boolean = passType && passMessage && passName;
         const expected = this.utils.printExpected(`new DOMException('${msg}', '${name}')`);
-        const actual = this.utils.printReceived(`new DOMException('${received.message}', '${received.name}')`);
+        const actual = this.utils.printReceived(`new ${constructor}('${received.message}', '${received.name}')`);
         const message: () => string = () => pass ? '' : `expected ${expected}\n but was ${actual}')`;
         return {
             message,
