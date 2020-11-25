328aa24053ab0e044fc5573a7748ef2835d7c089
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-11-25 21:41:22 +0100

    first draft of an api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,54 @@ export { MediaDevicesFake } from './MediaDevicesFake';
 export { MediaDeviceDescription } from './MediaDeviceDescription';
 export { anyMicrophone, anyCamera, anyDevice } from './DeviceMother';
 export { PermissionPrompt, PermissionPromptAction, RequestedMediaInput } from './UserConsentTracker';
+import { MediaDevicesFake } from './MediaDevicesFake';
+import { PermissionPrompt, PermissionState, UserConsent, UserConsentTracker } from './UserConsentTracker';
+import { MediaDeviceDescription } from './MediaDeviceDescription';
+
+export type InitialSetup = {
+    attachedDevices?: MediaDeviceDescription[];
+    microphone?: PermissionState;
+    camera?: PermissionState;
+}
+
+export interface MediaDevicesControl {
+    mediaDevices: MediaDevices;
+
+    attach(toAdd: MediaDeviceDescription): void
+
+    remove(toRemove: MediaDeviceDescription): void
+
+    deviceAccessPrompt(): Promise<PermissionPrompt>;
+}
+
+export const forgeMediaDevices = (initial: InitialSetup = {}): MediaDevicesControl => {
+    const attachedDevices = initial.attachedDevices ?? [];
+    const camera = initial.camera ?? PermissionState.Ask;
+    const microphone = initial.microphone = PermissionState.Ask;
+    const consent: UserConsent = { camera, microphone };
+    const consentTracker = new UserConsentTracker(consent);
+    const mediaDevicesFake = new MediaDevicesFake(consentTracker);
+    attachedDevices.forEach(device => mediaDevicesFake.attach(device));
+
+    return new class implements MediaDevicesControl {
+        get mediaDevices(): MediaDevices {
+            return mediaDevicesFake;
+        };
+
+        attach(toAdd: MediaDeviceDescription): void {
+            mediaDevicesFake.attach(toAdd);
+        }
+
+        remove(toRemove: MediaDeviceDescription): void {
+            mediaDevicesFake.remove(toRemove);
+        }
+
+        deviceAccessPrompt(): Promise<PermissionPrompt> {
+            return consentTracker.deviceAccessPrompt();
+        }
+    };
+};
+
 
 // todo testrig should be moved here
 export { MediaStreamCheckResult, Scenario } from './Scenarios';
